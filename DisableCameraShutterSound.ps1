08ff223a8f55aff44309f3a4764f2e18dc7811f9
admin62 <145646364+admin62@users.noreply.github.com>
2024-06-07 10:52:38 +0900

    Update DisableCameraShutterSound.ps1
    get privileges correctly.

--- DisableCameraShutterSound.ps1
+++ DisableCameraShutterSound.ps1
@@ -1,11 +1,20 @@
-# Get administrator authority
+# Get the script directory
+$scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+$scriptPath = Join-Path -Path $scriptDir -ChildPath "DisableCameraShutterSound.ps1"
+
 function Ensure-Admin {
-    $currentUser = [Security.Principal.WindowsIdentity]::GetCurrent()
-    $principal = New-Object Security.Principal.WindowsPrincipal($currentUser)
-    if (-not $principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-        # 관리자 권한이 없는 경우, 관리자 권한으로 스크립트 재실행 요청
-        Write-Host "관리자 권한이 필요합니다. 관리자 권한으로 스크립트를 다시 실행합니다."
-        Start-Process powershell -Verb runAs -ArgumentList ("-NoProfile -ExecutionPolicy Bypass -File `"" + $MyInvocation.MyCommand.Path + "`"")
+    try {
+        $currentUser = [Security.Principal.WindowsIdentity]::GetCurrent()
+        $principal = New-Object Security.Principal.WindowsPrincipal($currentUser)
+        if (-not $principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+            Write-Host "Administrator privileges are required. Attempting to restart the script with administrator privileges..."
+            Start-Process powershell.exe -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$scriptPath`"" -Verb RunAs
+            Exit
+        }
+    } catch {
+        Write-Host "An error occurred trying to run the script with administrator privileges. Error: $_"
+        # Wait for user input
+		Pause
         Exit
     }
 }
@@ -13,13 +22,11 @@ function Ensure-Admin {
 # Check Administrator authority
 Ensure-Admin
 
-# Get the script directory
-$scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
 $adbPath = Join-Path -Path $scriptDir -ChildPath "adb.exe"
 
 # executing 'adb devices' and logging the result
 Write-Host "Executing adb devices at $(Get-Date)"
-$adbDevicesOutput = adb devices
+$adbDevicesOutput = & $adbPath devices
 Write-Host $adbDevicesOutput
 
 # analyzing the 'adb devices' result
@@ -29,13 +36,18 @@ if ($adbDevicesOutput -like "*unauthorized*") {
     Write-Host "Device is authorized. Proceeding with adb command."
     # executing adb command and checking the result
     try {
-        adb shell settings put system csc_pref_camera_forced_shuttersound_key 0
-        $adbCommandOutput = adb shell settings get system csc_pref_camera_forced_shuttersound_key
+        $adbCommandOutput = & $adbPath shell settings get system csc_pref_camera_forced_shuttersound_key
+        if ($adbCommandOutput -eq "1") {
+            & $adbPath shell settings put system csc_pref_camera_forced_shuttersound_key 0
+            $adbCommandOutput = & $adbPath shell settings get system csc_pref_camera_forced_shuttersound_key
 
-        if ($adbCommandOutput -eq "0") {
-            Write-Host "Camera shutter sound disabled successfully."
+            if ($adbCommandOutput -eq "0") {
+                Write-Host "Camera shutter sound disabled successfully."
+            } else {
+                Write-Host "Failed to disable camera shutter sound. The current value is: $adbCommandOutput"
+            }
         } else {
-            Write-Host "Failed to disable camera shutter sound. The current value is: $adbCommandOutput"
+            Write-Host "Camera shutter sound is already disabled."
         }
     } catch {
         Write-Host "An error occurred while executing the adb command: $_"
